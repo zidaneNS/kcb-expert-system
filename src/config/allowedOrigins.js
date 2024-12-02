52be8361f5d22c0d468cf050f1708a9065258571
unknown <zidane.nur004@gmail.com>
2024-12-02 15:47:10 +0700

    bismillah bisa

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://sigapsehat.vercel.app/'
+    'https://sigapsehat.vercel.app'
 ];
 
 module.exports = allowedOrigins;
